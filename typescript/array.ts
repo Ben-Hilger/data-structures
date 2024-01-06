8afe00141ca661ea6f73eeb8c1e00fb70083aff3
Ben Hilger <bhilger78@gmail.com>
2024-01-05 21:59:06 -0500

    add basic array

---
+++ typescript/array.ts
@@ -0,0 +1,19 @@
+const a = new ArrayBuffer(6);
+console.log(a);
+
+const a8 = new Uint8Array(a);
+
+a8[0] = 45;
+a8[2] = 45;
+
+console.log(a);
+
+const a16 = new Uint16Array(a);
+
+a16[2] = 0x4545;
+
+console.log(a);
+
+a16[2] = 0x45;
+
+console.log(a);
